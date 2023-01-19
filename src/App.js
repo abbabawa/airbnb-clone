c8d4e1309dde47a43e6a42106b19fbc87ab99bf7
Abba Bawa <abbabawa6@gmail.com>
2023-01-19 08:59:07 +0100

    options Tab created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Box, CssBaseline } from "@mui/material";
 import Header from "./components/Header";
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import OptionsTab from "components/OptionsTab";
 
 // const router = createBrowserRouter([
 //   {
@@ -20,6 +21,7 @@ function App() {
       <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
         <Box>
           <Header />
+          <OptionsTab />
         </Box>
       </Box>
     </React.Fragment>
